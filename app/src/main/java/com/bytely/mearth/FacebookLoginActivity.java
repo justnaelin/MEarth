158e7116a5b0573a25f45d2672917bc304fb2c1d
yarelyc <yarely2011@yahoo.com>
2015-04-28 15:59:21 -0700

    Started working with the Facebook SDK

---
+++ app/src/main/java/com/bytely/mearth/FacebookLoginActivity.java
@@ -0,0 +1,22 @@
+package com.bytely.mearth;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by yaya on 4/28/15.
+ */
+public class FacebookLoginActivity extends ActionBarActivity {
+
+    void FacebookLoginActivity(){
+
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.facebook_activity);
+
+
+    }
+}
