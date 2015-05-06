23149066e7ba0abd72bbb6c0fedcfa8fe2224b10
Anita Garcia <anitagarcia819@gmail.com>
2015-05-05 18:03:38 -0700

    Added Facebook SDK login code

--- app/src/main/java/com/bytely/mearth/AboutFragment.java
+++ app/src/main/java/com/bytely/mearth/AboutFragment.java
@@ -1,5 +1,5 @@
 package com.bytely.mearth;
-
+// FYI to get profile picture, see video @ 12:30
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,11 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -23,13 +28,19 @@ import com.facebook.login.widget.LoginButton;
 
 public class AboutFragment extends Fragment {
     private Communicator comm;
+
+    private TextView mTextDetails;
     private CallbackManager mCallbackManager;
+
+    private AccessTokenTracker mTokenTracker;
+    private ProfileTracker mProfileTracker;
     private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
 
         @Override
         public void onSuccess(LoginResult loginResult) {
-
-// At 11:11
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile(); // Allows access to the person's profile
+            displayWelcomeMessage(profile);
         }
 
         @Override
@@ -50,10 +61,37 @@ public class AboutFragment extends Fragment {
     // Facebook Code to Log in (Temporarily Placed)
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
         mCallbackManager = CallbackManager.Factory.create();
+
+        // Will handel user name change.
+        AccessTokenTracker tracker = new AccessTokenTracker()
+        {
+            @Override
+
+            protected void onCurrentAccesTokenChanged(AccessToken old, AccessToken newToken){
+
+            }
+
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken accessToken, AccessToken accessToken2) {
+
+            }
+        };
+        // Track user profile changes
+        ProfileTracker profileTracker = new ProfileTracker(){
+
+
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+
+            }
+        };
+        tracker.startTracking();
+        profileTracker.startTracking();
+
 
     }
 
@@ -67,12 +105,32 @@ public class AboutFragment extends Fragment {
         return view;
     }
 
+    private void displayWelcomeMessage(Profile profile){
+
+        if (profile != null) {
+            mTextDetails.setText("Welcome " + profile.getName());
+
+        }
+    }
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
         loginButton.setReadPermissions("users_friends");//only ask persion if needed
         loginButton.setFragment(this);
+        loginButton.registerCallback(mCallbackManager, mCallback);
+
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Profile profile=Profile.getCurrentProfile();
+        displayWelcomeMessage(profile);
+
+
     }
 
     @Override
